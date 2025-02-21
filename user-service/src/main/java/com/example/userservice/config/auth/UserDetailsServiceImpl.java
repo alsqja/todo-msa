4ff2ff38cc610ebc8c99c0410e661812766c3b3f
alsqja <alsqha2626@gmail.com>
2025-02-21 10:23:21 +0900

    fix security userDetailsImpl

--- user-service/src/main/java/com/example/userservice/config/auth/UserDetailsServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/config/auth/UserDetailsServiceImpl.java
@@ -19,7 +19,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 
         return new UserDetailsImpl(user);
